2f5ad3819cb1ea9c51b9ccdd114062c1cdee3e2d
AdrLop <adrianlopianowski@gmail.com>
2025-01-01 22:47:53 +0100

    Added basic functionality for Task Tracker CLI

--- TaskTrackerCLI/task_cli.py
+++ TaskTrackerCLI/task_cli.py
@@ -6,15 +6,47 @@ import os
 import sys
 
 def main():
-    pass
+    database = load_database()
+    
+    parser = ArgumentParser(description="Task Tracker CLI")
+    parser.add_argument("command", choices=["add"], help="Action to perform")
+    parser.add_argument("--description", help="Description of the task (for 'add')", required=True)
+    args = parser.parse_args()
+
+    if args.command == "add":
+        add_task(database, args.description)
+        save_database(database)
+        print("Task added successfully!")
+
 def load_database():
-    pass
-def save_database():
-    pass
-def add_task():
-    pass
+    """Load database from JSON file."""
+    try:
+        with open('tasks.json', 'r') as file:
+            database = json.load(file)
+    except FileNotFoundError:
+        database = {}
+    return database
+
+def save_database(database):
+    """Save the database to a JSON file."""
+    with open("tasks.json", 'w') as file:
+        json.dump(database, file)
+
+def add_task(database, description):
+    """Add a new task to the database."""
+    id = str(max(map(int, database.keys()), default=0) + 1)
+    timer = datetime.now().isoformat()
+
+    database[id] = {
+        "description": description,
+        "status": "todo",
+        "created_at": timer,
+        "updated_at": timer,
+    }
+
 def update_task():
     pass
+
 def list_task():
     pass
 if __name__ == "__main__":
